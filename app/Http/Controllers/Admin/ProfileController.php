9816a27866ab42741370e0dfecda173aaa102762
Kato Goro <m01411027@iot.ac.jp>
2021-02-25 09:47:39 +0000

    PHP/Laravel 08-4

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,6 +17,7 @@ class ProfileController extends Controller
     public function edit(){
     	return view('admin.prohile.edit');
     }
+    
     public function update(){
     	return redirect('admin/profile/edit');
     }
