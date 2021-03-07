55045f118d323a38ebd696e88ca368b1ae7ef1c7
Kato Goro <m01411027@iot.ac.jp>
2021-03-07 02:59:47 +0000

    Routeing 追加

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,15 @@ Route::group(['prefix'=>'admin'],function(){
 	Route::get('profile/edit','Admin\ProfileController@edit');
 });
 Route::get('admin/news/create','Admin\NewsController@add');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'],function(){
+	Route :: get('news/create','Admin\NewsController@add') -> middleware('auth');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', 'HomeController@test')->name('test');
